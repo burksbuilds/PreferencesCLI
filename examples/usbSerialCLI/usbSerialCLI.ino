71528b81a94dc0e44b73293c0fe9a0c86a069fc9
Andrew Burks <86085214+burksbuilds@users.noreply.github.com>
2023-10-03 18:34:07 -0400

    Added Comments to usbSerial Example

--- examples/usbSerialCLI/usbSerialCLI.ino
+++ examples/usbSerialCLI/usbSerialCLI.ino
@@ -8,6 +8,7 @@
 #include "SimpleCLI.h"
 #include "PreferencesCLI.h"
 
+//Initialize global objects
 Preferences preferences;
 SimpleCLI cli;
 PreferencesCLI prefCli(preferences);
@@ -15,10 +16,14 @@ PreferencesCLI prefCli(preferences);
 void setup() {
   Serial.begin(115200);
   Serial.print("> ");
+
+  //register all of the preference related commands with the CLI. Other commands could be registered for other reasons too.
   prefCli.registerCommands(cli);
 }
 
 void loop() {
+
+  //Check if the user has sent text in via serial, then have the CLI parse it as a command
   if(Serial.available())
   {
     String line = Serial.readStringUntil('\n');
@@ -26,26 +31,34 @@ void loop() {
     cli.parse(line);
   }
 
+  //If the user input was successfulyl parsed by the CLI as a known command then it will be available in the CLI's queue
   if(cli.available())
   {
     bool handled = false;
+    //'pop' the first command from the CLI parsed command queue
     Command command = cli.getCommand();
+    //check to see if the command is related to the preferences library and handle it if it is.
+    //The command might not be for the preferences library, so you may need to go 'ask' other parts of your code if they can handle this command.
     handled &= prefCli.handleCommand(command, Serial);
     Serial.print("\n> ");
   }
 
+  //if the input passed into the CLI parser is not a valid command then it can be popped off the error queue
   if(cli.errored())
   {
     CommandError e = cli.getError();
     handleCliError(e, Serial);
   }
 }
+
+//this function handles displaying an error message over serial if the text sent does not parse as a known, valid command
 void handleCliError(CommandError &e, Stream &outputStream)
 {
   CommandError cmdError(e);
   outputStream.print("ERROR: ");
   outputStream.println(cmdError.toString());
 
+  //This is the case where the CLI was smart enough to know what command was requested, but the parameters sent with the command don't make sense for that command
   if (cmdError.hasCommand()) {
       outputStream.println(cmdError.getCommand().toString());
   }
